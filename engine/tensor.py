Fix several bugs (broadcasting mismatch, didn't force floats)

engine/tensor.py
@@ -10,7 +10,7 @@ import numpy as np
 
 class Tensor:
     def __init__(self, data):
-        self.data = np.array(data)
+        self.data = np.array(data, dtype=float)
         # gradient is initialised to zero, will be updated during backpropagation, ensure its the same shape
         self.grad = np.zeros_like(self.data) 
         self._backward = lambda: None
@@ -54,9 +54,8 @@ class Tensor:
                 if dim == 1:
                     grad_other = grad_other.sum(axis = index, keepdims = True)
 
-
-            self.grad += new.grad
-            other.grad += new.grad
+            self.grad += grad_self
+            other.grad += grad_other
         
         new._backward = _backward
 

examples/xor.py
@@ -16,7 +16,7 @@ def main():
     # output layer: 1 neuron (0 or 1)
     w1 = Tensor(np.random.randn(2, 4) * 0.1)
     b1 = Tensor(np.zeros((1,4)))
-    w2 = Tensor(np.random.randn(2, 4) * 0.1)
+    w2 = Tensor(np.random.randn(4, 1) * 0.1)
     b2 = Tensor(np.zeros((1,4)))
 
     params = [w1, b1, w2, b2]