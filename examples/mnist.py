5c35a48c4678eda778429ba53cc3cc2bddadbdc9
Sean Zeng <seandoesstuff602@gmail.com>
2026-03-08 18:38:27 +1100

    Add MNIST example

---
+++ examples/mnist.py
@@ -0,0 +1,50 @@
+import numpy as np
+from sklearn.datasets import fetch_openml
+from engine.tensor import Tensor
+import engine.nn as nn
+import engine.optimisers as optim
+
+print("Fetching MNIST dataset...")
+mnist = fetch_openml('mnist_784', version=1, cache=True, parser='auto')
+
+# Divide X by 255 so the raw values are normalised between 0 and 1 (so gradrients don't explode)
+X_raw = mnist.data.to_numpy() / 255.0
+Y_raw = mnist.target.to_numpy().astype(int)
+
+num_samples = X_raw.shape[0]
+Y_encoded = np.zeros((num_samples, 10))
+Y_encoded[np.arange(num_samples), Y_raw] = 1.0
+
+model = nn.Sequential(
+    nn.Linear(784, 128),
+    nn.ReLu(),
+    nn.Linear(128, 10)
+)
+
+optimiser = optim.SGD(model.parameters(), lr = 0.1)
+
+batch_size = 128
+
+print("Start training \n")
+for i in range(1001):
+    batch = np.random.randint(0, num_samples, size=batch_size)
+
+    X_batch = Tensor(X_raw[batch])
+    Y_batch = Tensor(Y_encoded[batch])
+
+    pred = model(X_batch)
+
+    loss = ((pred - Y_batch) * (pred - Y_batch)).sum() / batch_size
+
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+    if i % 100 == 0:
+        predictions = np.argmax(pred.data, axis = 1)
+        targets = np.argmax(Y_batch.data, axis = 1)
+        accuracy = np.mean(predictions == targets) * 100
+
+        print(f"Iteration {i:4d} | Loss: {loss.data:.4f} | Batch Accuracy: {accuracy:.2f}%")
+
+print("\nTraining complete\n")
--- examples/xor.py
+++ examples/xor.py
@@ -7,52 +7,46 @@ from engine.tensor import Tensor
 import engine.nn as nn
 import engine.optimisers as optim
 
-def main():
-    np.random.seed(67)
+np.random.seed(67)
 
-    X = Tensor([[0, 0], [0, 1], [1, 0], [1, 1]]) # inputs
-    Y = Tensor([[0], [1], [1], [0]]) # targets
+X = Tensor([[0, 0], [0, 1], [1, 0], [1, 1]]) # inputs
+Y = Tensor([[0], [1], [1], [0]]) # targets
 
-    model = nn.Sequential(
-        nn.Linear(2, 16),
-        nn.ReLu(),
-        nn.Linear(16, 1)
-    )
+model = nn.Sequential(
+    nn.Linear(2, 16),
+    nn.ReLu(),
+    nn.Linear(16, 1)
+)
 
-    learning_rate = 0.025
+learning_rate = 0.025
 
-    # input layer: 2 neurons (since 2 things to change)
-    # hidden layer: 16 neurons
-    # output layer: 1 neuron (0 or 1)
+# input layer: 2 neurons (since 2 things to change)
+# hidden layer: 16 neurons
+# output layer: 1 neuron (0 or 1)
 
-    optimiser = optim.SGD(model.parameters(), lr = learning_rate)
+optimiser = optim.SGD(model.parameters(), lr = learning_rate)
 
-    print("Start training \n")
+print("Start training \n")
 
-    for i in range(100):
-        pred = model(X)
+for i in range(100):
+    pred = model(X)
 
-        # mean squared error for loss
-        loss = ((pred - Y) * (pred - Y)).sum()
+    # mean squared error for loss
+    loss = ((pred - Y) * (pred - Y)).sum()
 
-        optimiser.zero_grad()
-        loss.backward()
-        optimiser.step()
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
 
-        for p in model.parameters():
-            p.data -= learning_rate * p.grad
+    for p in model.parameters():
+        p.data -= learning_rate * p.grad
 
-        if i % 10 == 0:
-            print(f"Iteration {i}: Loss: {loss.data:.4f}")
+    if i % 10 == 0:
+        print(f"Iteration {i}: Loss: {loss.data:.4f}")
 
-    print("\nTraining complete\n")
-    print("Final Predictions: (target is [0, 1, 1, 0]):\n")
+print("\nTraining complete\n")
+print("Final Predictions: (target is [0, 1, 1, 0]):\n")
 
-    final_pred = model(X)
-    print(np.round(final_pred.data, 3))
-
-if __name__ == "__main__":
-    main()
-
-
+final_pred = model(X)
+print(np.round(final_pred.data, 3))
 
